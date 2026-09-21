091231045c9bd0bc19ec6ebbc123e90b434aadec
Kamran Azim <Kamazim121212@gmail.com>
2026-09-21 11:43:19 +0100

    Add number pad
    - Loop that creates button with flexbasis equal to
      a third of the pad container
    - Creates 3 buttons a row until 9 which is on its own
    - Use aspect ratio to ensure buttons have equal height and width

---
+++ calc.js
@@ -0,0 +1,19 @@
+const numContainer = document.querySelector(".num-container");
+const opContainer = document.querySelector(".operations-container");
+
+const numbersArray = [7,8,9,4,5,6,1,2,3,0];
+const othersArray = [".", "="];
+const operationsArray = ["÷", "x", "-", "+"]
+const undoButtonsArray = ["<---" , "AC"]
+
+
+function createNumberPad() {
+    for (let i = 0; i < numbersArray.length; i++) {
+        const square = document.createElement("button");
+        square.className = "square";
+        numContainer.appendChild(square);
+    }
+}
+
+
+createNumberPad();
